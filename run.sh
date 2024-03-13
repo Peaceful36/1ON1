f7fe8213a041ac076d1fb0f43a5d26c9266ec6dd
ACER <hoathai.nguyen@mail.utoronto.ca>
2024-03-12 23:04:15 -0400

    added startup.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,3 @@
-# Install python interpreter
-sudo apt-get install python3
-# Install pip
-sudo apt-get install python3-pip
-# Create virtual environment
-python3 -m venv venv
-# Activate virtual environment
-source venv/bin/activate
-# Install dependencies
-pip install -r requirements.txt
+# run application
+python ./manage.py runserver
 ```
